c167969eb8b412811a42852c50d7e4e2acbdce71
Meow <phlamedev@gmail.com>
2020-05-06 16:56:48 -0400

    add setstatus command

---
+++ cmds/setstatus.js
@@ -0,0 +1,18 @@
+const Discord = module.require("discord.js");
+
+module.exports.run = async (bot, message, args, cleanArgs) => {
+  if (message.member.isAdmin()) {
+    bot.user.setStatus(args[0]);
+    message.channel.send("I have set the status to " + args[0] + "!");
+  } else {
+    message.reply("you are not a server admin!");
+  }
+};
+module.exports.help = {
+  name: "setstatus",
+  description:
+    "Sets the bot's status to one of `online`, `idle`, `dnd`, and `invisible`.",
+  usage: "setstatus [status]",
+  type: "utility",
+  commandAliases: ["status"],
+};
