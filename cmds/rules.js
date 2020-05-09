1f468d2ec9c99780965f05ea723f5e7fa56f2b62
Meow <phlamedev@gmail.com>
2020-05-08 20:30:41 -0400

    add rules command

---
+++ cmds/rules.js
@@ -0,0 +1,21 @@
+const Discord = module.require("discord.js");
+
+module.exports.run = async (bot, message, args, cleanArgs) => {
+  message.delete();
+  if (message.member.isAdmin()) {
+    let channel = message.mentions.channels.first() || message.channel;
+    channel.send(bot.embeds.rules);
+    message.channel
+      .send("Rules sent to " + channel + "!")
+      .then((msg) => msg.delete(4000));
+  } else {
+    message.reply("you are not a bot admin!");
+  }
+};
+module.exports.help = {
+  name: "rules",
+  description: "Sends the rules in the specified channel.",
+  usage: "rules <channel>",
+  type: "admin",
+  commandAliases: [],
+};
