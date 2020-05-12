36c2e3cbacb397981374ec861ea0cc251b23caff
Meow <phlamedev@gmail.com>
2020-05-11 20:50:21 -0400

    add toString for hex color conversion

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ app.get("/user/:id", (req, res) => {
         tag: member.user.tag,
         avatarURL: member.user.displayAvatarURL,
         nickname: member.nickname,
-        color: (member.colorRole || {}).color,
+        color: ((member.colorRole || {}).color || "").toString(16),
         roles: member.roles.map((r) => r.name),
         colorRole: (member.colorRole || {}).name,
       };
