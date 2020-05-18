82705ff569b7e58795a4f802b2f34cb23a99b824
Meow <erty2pop@gmail.com>
2020-05-18 15:31:03 -0400

    comment

--- cmds/eval.js
+++ cmds/eval.js
@@ -1,17 +1,17 @@
 const Discord = module.require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  if (message.author.id !== "521701910609133570") return;
-  let code = args.slice(1).join(" ");
-  let done = "Nothing 3:";
+  if (message.author.id !== "521701910609133570") return; // stop if meow isnt running the command
+  let code = args.slice(1).join(" "); // get the code
+  let done = "Nothing 3:"; // declare the evaluated code
 
   try {
-    done = eval(code);
+    done = eval(code); // eval the code
   } catch (e) {
-    done = e;
+    done = e; // catch any errors
   }
 
-  message.channel.send("```js\n" + code + "``````" + done + "```");
+  message.channel.send("```js\n" + code + "``````" + done + "```"); // send the code
 };
 module.exports.help = {
   name: "eval",
