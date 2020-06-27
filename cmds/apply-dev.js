b30a3f796ab0c69d3924c5ca7e241b291e5aac68
Enosh Lartey <55937921+EnoshLartey@users.noreply.github.com>
2020-06-26 23:55:30 -0600

    Create apply-dev.js

---
+++ cmds/apply-dev.js
@@ -0,0 +1,16 @@
+const Discord = module.require("discord.js");
+const snekfetch = require("snekfetch");
+
+module.exports.run = async (bot, message, args, cleanArgs) => {
+  
+  } catch (err) {
+    return console.log(err);
+  }
+};
+module.exports.help = {
+  name: "apply-dev",
+  description: "Apply for the developer role.",
+  usage: "apply-dev",
+  type: "development",
+  commandAliases: ["developer"],
+};
