c31d68424863bfd1a5445c47345b3c6ca8599500
Meow <erty2pop@gmail.com>
2020-08-18 00:36:18 -0400

    Create ban.js

---
+++ cmds/ban.js
@@ -0,0 +1,38 @@
+const Discord = module.require("discord.js");
+
+module.exports.run = async (bot, message, args, cleanArgs) => {
+  let member = message.mentions.members.first();
+  if (!member) return message.channel.send("Please specify a member to ban!");
+
+  let reason = args.slice(2).join(" ") || "No reason provided.";
+
+  if (member.id == message.member.id) return message.channel.send("You can not ban yourself!");
+
+  if (message.member.hasPermission("BAN_MEMBERS")) {
+    if (!message.guild.me.hasPermission("BAN_MEMBERS"))
+      return message.channel.send("I do not have the BAN_MEMBERS permission!");
+    if (!member.bannable) return message.channel.send("I can not ban this member!");
+    if (
+      member.highestRole.comparePositionTo(message.member.highestRole) >= 0 &&
+      message.guild.owner.id !== message.member.id
+    )
+      return message.channel.send("You can not ban this member! They have too high of a role.");
+
+    await member.user
+      .send(`You have been banned from ${message.guild.name} for **${reason}**.`)
+      .catch((err) => {
+        message.channel.send("I couldn't message the user.");
+      });
+    await member.ban(reason);
+    await message.channel.send(`Banned ${member.toString()} (ID: ${member.id}) for **${reason}**.`);
+  } else {
+    message.channel.send("You do not have permission to ban members!");
+  }
+};
+module.exports.help = {
+  name: "ban",
+  description: "Bans a member from the server.",
+  usage: "ban [user] <reason>",
+  type: "moderation",
+  commandAliases: ["b"],
+};
