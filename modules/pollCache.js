d1f4fafe9afa37e57aa86c7a570aa93063d94a6d
Meow <50887230+itzTheMeow@users.noreply.github.com>
2020-12-26 03:59:05 -0500

    fix

--- modules/pollCache.js
+++ modules/pollCache.js
@@ -3,11 +3,12 @@ module.exports = function (bot) {
     let activePolls = bot.db.fetch(`polls_${g.id}`) || [];
     if (!activePolls.length) return;
 
-    activePolls.forEach((p) => {
-      let pollChannel = g.channels.cache.get(p.split(":")[0]);
+    activePolls.forEach(async (p) => {
+      let pollChannel = await g.channels.cache.get(p.split(":")[0]);
       if (!pollChannel) return;
-      let poll = pollChannel.messages.fetch(p.split(":")[1]);
+      let poll = await pollChannel.messages.fetch(p.split(":")[1]);
       if (!poll) return;
+      poll.reactions.cache.map((r) => r.users.fetch());
     });
   });
 };
