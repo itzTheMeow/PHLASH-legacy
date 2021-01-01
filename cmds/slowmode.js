5093ed2adfb36e4e4ad7ba0e0a4efe9206b58da7
PandaCrazy2529 <53063677+PandaCrazy2529@users.noreply.github.com>
2021-01-01 16:31:25 -0700

    Create slowmode.js

---
+++ cmds/slowmode.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+const i4h = require("intervals-for-humans");
+
+module.exports.run = async (client, message, args) => {
+  if (!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("You must have `MANAGE_CHANNELS` permission to use this command!");
+  if (!args[1]) return message.channel.send("Please specify the new slowmode (in seconds)!");
+  let time = args[1];
+  if (time.toLowerCase() == "off") {
+    time = "0s";
+  }
+  time = i4h(time);
+  if (!time || (time < 1000 && time != 0) || (time / 1000) != Math.floor(time / 1000)) return message.channel.send("That's not a valid time!");
+  time = time / 1000;
+  if (time > 21600) return message.channel.send("Slowmode can't be longer than 6 hours!");
+  try {
+    await message.channel.setRateLimitPerUser(time, "Requested by " + message.author.tag + " (ID: " + message.author.id + ")");
+  } catch(e) {
+    message.channel.send("An error occurred when setting slowmode. Please check my permissions and try again.");
+    return;
+  }
+  if (time == 0) {
+    message.channel.send("I have turned off slowmode in this channel!");
+  } else {
+    message.channel.send("I have set slowmode for this channel!!");
+  }
+};
+
+module.exports.help = {
+  name: "slowmode",
+  alias: [
+    "setslowmode"
+  ]
+};
