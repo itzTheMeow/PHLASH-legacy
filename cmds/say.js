09956d4100d89039790ae4174fe94c9bfeb20ca5
PandaCrazy2529 <53063677+PandaCrazy2529@users.noreply.github.com>
2021-01-03 18:49:02 -0700

    Update say.js

--- cmds/say.js
+++ cmds/say.js
@@ -1,11 +1,11 @@
-/* const Discord = module.require("discord.js");
+const Discord = module.require("discord.js");
 
 module.exports.run = async (bot, message, args, cleanArgs) => {
   message.delete();
   if (message.member.isAdmin()) {
     message.channel.send(args.slice(1).join(" "));
   } else {
-    message.channel.send(cleanArgs.slice(1).join(" "));
+    message.channel.send(args.slice(1).join(" "), { disableMentions: "all" });
   }
 };
 module.exports.help = {
@@ -15,27 +15,3 @@ module.exports.help = {
   type: "fun",
   commandAliases: ["repeat", "speak"],
 };
- */
-
-const Discord = require('discord.js');
-
-module.exports.run = async (client, message, args) => {
-  if (!args[1]) return message.channel.send("Please specify text to say!");
-  let text = args.slice(1).join(" ");
-  let json;
-  try {
-    json = JSON.parse(text);
-  } catch (err) {
-    message.channel.send(text);
-    return;
-  }
-  let embed = new Discord.MessageEmbed(json);
-  message.channel.send((json.content || ""), embed);
-}
-
-module.exports.help = {
-  name: "say",
-  description: "Replies with the text you say.",
-  type: "fun",
-  commandAliases: [ "embed" ]
-}
