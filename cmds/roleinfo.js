1bf7a818fda445b56e754ef1887c3826b72cac31
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-01-05 16:24:04 -0500

    format

--- cmds/roleinfo.js
+++ cmds/roleinfo.js
@@ -1,20 +1,26 @@
 const Discord = require("discord.js");
 
 const extr = require("tcc-cdn");
-let texttools = extr('texttools');
+let texttools = extr("texttools");
 texttools.use();
 
 module.exports.run = async (client, message, args) => {
   if (!args[1]) return message.channel.send("Please specify a role to show info on!");
   let role = message.mentions.roles.first();
   if (!role) {
-    role = message.guild.roles.cache.filter(role => role.name.toLowerCase().includes(args.slice(1).join(" ").toLowerCase()));
-    if (role.size == 0) return message.channel.send("I can't find any roles that match that search!");
-    if (role.size >= 2) return message.channel.send("Hmm... I found multiple results for that role. Try being more specific?");
+    role = message.guild.roles.cache.filter((role) =>
+      role.name.toLowerCase().includes(args.slice(1).join(" ").toLowerCase())
+    );
+    if (role.size == 0)
+      return message.channel.send("I can't find any roles that match that search!");
+    if (role.size >= 2)
+      return message.channel.send(
+        "Hmm... I found multiple results for that role. Try being more specific?"
+      );
     role = role.first();
   }
-  let mentionable = role.mentionable ? "Yes" : "No"
-  let hoisted = role.hoisted ? "Yes" : "No"
+  let mentionable = role.mentionable ? "Yes" : "No";
+  let hoisted = role.hoisted ? "Yes" : "No";
   let permissions = role.permissions.serialize();
   let permissionsArray = [];
   for (let key in permissions) {
@@ -42,5 +48,5 @@ module.exports.help = {
   description: "Gives you info about a role.",
   usage: "roleinfo [role]",
   type: "utility",
-  comandAliases: [ "ri", "role" ]
+  commandAliases: ["ri", "role"],
 };
