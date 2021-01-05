5ff653c77d3c11802427c6d14e122ba5f3754cde
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-01-05 16:24:21 -0500

    simplify/format

--- cmds/embed.js
+++ cmds/embed.js
@@ -1,31 +1,42 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
-module.exports.run = async (client, message, args) => {
-  if (!args[1]) return message.channel.send("Please specify embed JSON!");
-  let json = args.slice(1).join(" ");
+module.exports.run = async (bot, message, args) => {
+  let json = args.slice(1).join(" ") || "";
   try {
-    json = JSON.parse(text);
+    json = JSON.parse(json);
   } catch (err) {
-    message.channel.send("Please specify valid embed JSON!");
+    return message.channel.send(`Please specify valid embed JSON! Error: ${err}`);
+  }
+
+  let embed = new Discord.MessageEmbed();
+  embed.setColor("#FFFFFF");
+  embed.setTitle("Failed to create embed.");
+  embed.setDescription(
+    "Failed to create embed due to invalid JSON. Check your JSON/generator and try again."
+  );
+
+  try {
+    embed = new Discord.MessageEmbed(json);
+  } catch (err) {
+    message.channel.send({ embed });
     return;
   }
-  try {
-    let embed = new Discord.MessageEmbed(json);
-  } catch (err) {
-    message.channel.send("Sorry, that JSON doesn't work as an embed due to Discord limitations.");
-    return;
-  }  
+
   if (message.member.isAdmin()) {
-    message.channel.send((json.content || ""), embed);
+    message.channel.send({ embed }).catch((err) => {
+      message.channel.send(String(err));
+    });
   } else {
-    message.channel.send((json.content || ""), embed, { disableMentions: "all" });
+    message.channel.send({ embed, disableMentions: "all" }).catch((err) => {
+      message.channel.send(String(err));
+    });
   }
-}
+};
 
 module.exports.help = {
   name: "embed",
   description: "Replies with the equivelant embed to the given JSON.",
   usage: "embed [embed json]",
   type: "fun",
-  commandAliases: []
-}
+  commandAliases: [],
+};
