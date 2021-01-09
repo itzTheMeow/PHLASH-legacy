312e02f169e64cbdbc9dbf22ecbbc6263b60a7b3
Meow <50887230+itzTheMeow@users.noreply.github.com>
2021-01-09 01:35:32 -0500

    add rule embed

--- Embeds.js
+++ Embeds.js
@@ -4,6 +4,15 @@ module.exports = function (bot) {
   return {
     Rules: new Discord.MessageEmbed()
       .setColor(bot.color)
-      .setAuthor(`${bot.guild.name} Rules`, bot.guild.iconURL()),
+      .setAuthor(`${bot.guild.name} Rules`, bot.guild.iconURL())
+      .setFooter("If you have any questions, please contact Meow.")
+      .addField(
+        "**1.** No spamming.",
+        "No spamming chat. This includes spamming CAPS, emojis, repeated messages, and jibberish."
+      )
+      .addField(
+        "**2.** No racism.",
+        "Racism is not tolerated on this server. This includes racial slurs, discrimination, etc."
+      ),
   };
 };
